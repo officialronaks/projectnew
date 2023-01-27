cc2ef8c6417811632bbe43acf3edb1d81b23e000
officialronaks <sronak922@gmail.com>
2023-01-28 00:16:35 +0530

    First commit without form

---
+++ Test 2/Test 2/Models/feedback.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Test_2.Models
+{
+    public class feedback
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public int Answer { get; set; }
+        [StringLength(500)]
+        public string Content { get; set; }
+        [StringLength(100)]
+        public string FullName { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+    }
+}
